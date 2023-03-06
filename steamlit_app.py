cb0f4bb3cd173897e8e5d90870b1b8d50f1557ba
Markgerald101 <127067678+Markgerald101@users.noreply.github.com>
2023-03-06 10:54:33 +0800

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Parent new healthy Diner')
 
 
@@ -7,6 +8,6 @@ streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
