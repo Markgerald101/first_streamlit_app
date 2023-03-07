3c96c7a750b2d7e66c94a46e3b5ddeacf349d78e
Markgerald101 <127067678+Markgerald101@users.noreply.github.com>
2023-03-07 15:18:31 +0800

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -3,5 +3,7 @@ import streamlit
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg') streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
+
 
